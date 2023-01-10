152395d93df3840848253c9e165c93c58e5fdacd
Lukasz-Milde <lukasz@milde.live>
2023-01-10 15:18:45 +0100

    working on js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("jazda z kurwami !");
+const url = "https://api.adviceslip.com/advice";
+const adviceNumber = document.querySelector(".advice-generator__number");
+const adviceSlot = document.querySelector(".advice");
+const btn = document.querySelector(".btn");
+
+const getAdvice = async () => {
+	const response = await fetch(url);
+	const advice = await response.json();
+	console.log(advice);
+	return advice;
+};
+
+const renderAdvice = (adviceObject) => {
+	const { id, advice } = adviceObject;
+	adviceNumber.textContent = `ADVICE #${id}`;
+	adviceSlot.textContent = `"${advice}"`;
+};
+
+const getNewAdvice = async () => {
+	const data = await getAdvice();
+	const advice = data.slip;
+	renderAdvice(advice);
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+	btn.addEventListener("click", getNewAdvice);
+});
---
+++ test.js
@@ -0,0 +1,4 @@
+const url = "https://api.adviceslip.com/advice";
+const adviceNumber = document.querySelector(".advice-generator__number");
+const adviceSlot = document.querySelector(".advice");
+const btn = document.querySelector(".btn");
